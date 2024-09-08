ced08641221be4ceab7af759c793f8e5f63d3a0a
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2024-09-08 21:00:23 +0530

    react randon image

---
+++ react_random_image_generate_login/src/Routes/Login.jsx
@@ -0,0 +1,72 @@
+import {useContext, useRef, useState} from "react";
+import { AppContext } from "../Context/AppContext";
+import { useNavigate,Link } from "react-router-dom";
+
+
+function Login() {
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("")
+  const {loginUser } = useContext(AppContext);
+  // const [ disable ,setDisable ]=useState(false)
+  const navigate = useNavigate();
+
+  const [ disableBtn , setDisableBtn ]=useState(false)
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    const payload ={
+      email,
+      password
+    }
+setDisableBtn(true)
+    fetch("https://reqres.in/api/login",{
+      method:"POST",
+      headers:{
+      "Content-Type" : "application/json"
+      },
+      body:JSON.stringify(payload)
+    }).then((res)=>{
+      return res.json()
+    }).then ((res)=>{
+      console.log(res.token)
+      loginUser(res.token);
+      setDisableBtn(false)
+      navigate("/dashboard")
+    })
+  }
+
+  return (
+    <div className="login-page" >
+      <form data-testid="login-form" className="form" onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <input data-testid="email-input" type="email" placeholder="email" value={email} 
+            onChange={(e)=>setEmail(e.target.value)}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              data-testid="password-input"
+              type="password"
+              placeholder="password" 
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+        <button  disabled={disableBtn} data-testid="form-submit" type="submit">
+            SUBMIT
+          </button>
+        </div>
+      </form>
+      <div className="message" >
+        <Link to="/">Go Back</Link>
+      </div>
+      </div>
+  );
+}
+export default Login;
