69970ad7cc3118797204f5b6e93e45888b0482d3
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2024-10-31 22:01:34 +0530

    add audio

--- dipawali_2024/src/component/dashboard.jsx
+++ dipawali_2024/src/component/dashboard.jsx
@@ -1,3 +1,4 @@
+import ReactAudioPlayer from "react-audio-player";
 import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
@@ -7,8 +8,13 @@ const Dashboard = () => {
   return (
 
     <div className=" dashboardBgContainer min-h-screen flex items-center justify-center ">
+       <ReactAudioPlayer
+        src="Aayi_gaye_.mp3"
+        autoPlay
+        loop
+      />
       
-    <div className=" flex items-center justify-center h-screen px-4 sm:px-8 md:px-12 lg:px-16  lg:-mt-52 sm: -mt-48 md: -mt-36  w-11/12 md:9/12 sm:3/6  font-[cursive]">
+    <div className=" flex items-center justify-center h-screen px-4 sm:px-8 md:px-12 lg:px-16  w-11/12 md:9/12 sm:3/6  font-[cursive]">
       <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold bg-cyan-500 rounded-2xl py-4 px-2 w-full sm:w-2/3 neon-glow-dashboard hover:neon-glow-dashboard border-double border-4  font-[cursive] text-white custom-text-shadow "> {name} की तरफ से आप सभी को दिवाली की हार्दिक शुभकामनाएँ </h1>
     </div>
     </div>
